c4c5199f644032d8c15c6e9f06f3b9acf61e8bca
Andreas Heim <andreas@andreas-laptop.(none)>
2009-11-30 09:49:53 +0100

    pulled changes, but removed install-lejos-jars (platform dependent) and bumped source version to 1.6

--- install-lejos-jars.sh
+++
@@ -1,6 +0,0 @@
-# installs the lejos jars in the local maven repo
-
-mvn install:install-file -DgroupId=org.lejos -DartifactId=classes -Dversion=0.85 -Dpackaging=jar -Dfile=classes.jar
-mvn install:install-file -DgroupId=org.lejos -DartifactId=jtools -Dversion=0.85 -Dpackaging=jar -Dfile=jtools.jar
-mvn install:install-file -DgroupId=org.lejos -DartifactId=pctools -Dversion=0.85 -Dpackaging=jar -Dfile=pctools.jar
-mvn install:install-file -DgroupId=org.lejos -DartifactId=pccomms -Dversion=0,85 -Dpackaging=jar -Dfile=pccomm.jar
